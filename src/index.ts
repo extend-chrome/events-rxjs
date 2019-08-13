fcee540907bb0414de65bbd5e2b57853d0af0a6e
Jack Steam <teamsteamdev@gmail.com>
2019-08-13 14:11:49 -0500

    Export tabs

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 /*            CHROME API OBSERVABLES            */
 /* ============================================ */
 
-export { action, browserAction } from './action'
+export { action, browserAction } from './action';
 // export { alarms } from './alarms';
-export { fromChromeEvent } from './fromChromeEvent'
-export { contextMenus, menus } from './menus'
-export { notifications } from './notifications'
+export { fromChromeEvent } from './fromChromeEvent';
+export { contextMenus, menus } from './menus';
+export { notifications } from './notifications';
 // export { proxy } from './proxy';
-export { runtime } from './runtime'
-export { storage } from './storage'
+export { runtime } from './runtime';
+export { storage } from './storage';
+export { tabs } from './tabs';
+
