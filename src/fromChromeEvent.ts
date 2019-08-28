74cac1462471eaab66fbf7a3943b45f4de9a2620
Jack Steam <teamsteamdev@gmail.com>
2019-08-28 09:59:18 -0500

    Fix fromChromeEvent

--- src/fromChromeEvent.ts
+++ src/fromChromeEvent.ts
@@ -9,26 +9,14 @@ import { fromEventPattern, Observable } from 'rxjs'
  */
 export function fromChromeEvent<T extends any[]>(
   event: chrome.events.Event<Function>,
-  resultSelector?: (...args: any[]) => T,
 ): Observable<T> {
-  if (resultSelector) {
-    return fromEventPattern(
-      (handler) => {
-        event.addListener(handler)
-      },
-      (handler) => {
-        event.removeListener(handler)
-      },
-      resultSelector,
-    )
-  } else {
-    return fromEventPattern(
-      (handler) => {
-        event.addListener(handler)
-      },
-      (handler) => {
-        event.removeListener(handler)
-      },
-    )
-  }
+  return fromEventPattern(
+    (handler) => {
+      event.addListener(handler)
+    },
+    (handler) => {
+      event.removeListener(handler)
+    },
+    (...args) => args as T,
+  )
 }
