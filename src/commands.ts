1b7caa9db51ee9615ac19dbb50c8d82e0312b57a
Jack Steam <teamsteamdev@gmail.com>
2019-10-24 14:18:54 -0500

    Add chrome.commands

---
+++ src/commands.ts
@@ -0,0 +1,25 @@
+import { fromChromeEvent } from './fromChromeEvent'
+
+/**
+ * Events for `chrome.commands`
+ *
+ * https://developer.chrome.com/extensions/commands#event-onCommand
+ */
+
+export const commands = {
+  // TODO: add commandStream
+
+  /**
+   * Observable from `chrome.commands.onCommand`
+   *
+   * Fired when a registered command is activated using a keyboard shortcut.
+   *
+   * https://developer.chrome.com/extensions/commands#event-onCommand
+   */
+  get commandStream() {
+    return fromChromeEvent<[string]>(
+      chrome.commands.onCommand,
+    )
+  },
+
+}
