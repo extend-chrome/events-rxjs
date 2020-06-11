1e8f98f63271a5e2fafc20c35ce8651f73e35a8a
Jack Steam <jacksteamdev@gmail.com>
2020-06-11 09:59:36 -0500

    Fix tests Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./tests/jest.setup.ts'],
+  globals: {
+    'ts-jest': {
+      packageJson: './package.json',
+      tsConfig: './tsconfig.json',
+    },
+  },
 }
